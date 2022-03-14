224cc83d93df61c423b0babf61ae06bf423a844e
kreangkrai <kreangkrai.ratt@gmail.com>
2022-03-14 17:28:07 +0700

    add Test

--- src/handlers/parts.rs
+++ src/handlers/parts.rs
@@ -25,3 +25,24 @@ pub async fn remove_part(_id :web::Path<String>)->Result<HttpResponse,Error>{
     let data = part::delete(_id.into_inner()).await;  
     Ok(HttpResponse::Ok().json(data.unwrap()))
 }
+
+#[cfg(test)]
+mod tests{
+    use crate::app_config::config_app;
+    use actix_web::dev::Service;
+    use actix_web::{http::{header,StatusCode},test,App,};
+
+    #[actix_web::test]
+    async fn test_add_part(){
+        let app = test::init_service(App::new().configure(config_app)).await;
+        let payload = r#"{"id":"P001","part_type":"funcy","name":"test"}"#.as_bytes();
+        let req = test::TestRequest::post()
+        .uri("/parts")
+        .insert_header((header::CONTENT_TYPE,"application/json"))
+        .set_payload(payload)
+        .to_request();
+
+        let resp = app.call(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::OK);
+    }
+}
